fc7cb88f930d3bad7d90de6a20071c2f0b97d818
BenC <bcuttance1998@gmail.com>
2023-12-18 19:32:47 +1030

    added basic 30 second timer that will count down on begining of game

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
-const startBtn = document.getElementById('startBtn')
-const gameBtn = document.getElementById('gameBtn')
-const restartBtn = document.getElementById('restartBtn')
+const startBtn = document.getElementById("startBtn");
+const gameBtn = document.getElementById("gameBtn");
+const restartBtn = document.getElementById("restartBtn");
 
 startGame = () => {
-startBtn.style.display = 'none'
-gameBtn.style.display = 'block'
-restartBtn.style.display = 'block'
-}
+  startBtn.style.display = "none";
+  gameBtn.style.display = "block";
+  restartBtn.style.display = "block";
+  myCountDown()
 
-playBtn = () => {
-const myTop = Math.floor(Math.random() * (850 - 85) + 85)
-const myLeft = Math.floor(Math.random() * (1730 - 0) + 0)
-newTopValue = myTop + 'px'
-newLeftValue = myLeft + 'px'
-console.log(newLeftValue)
-gameBtn.style.top = newTopValue
-gameBtn.style.left = newLeftValue
-
-
-}
-
-var i = 0 
-scoreFunction = () => {
-document.getElementById('myScore').value = ++i;
-}
-
-displayScoreBoard = () => {
-
+  setInterval(myCountDown, 1000);
 };
 
-restartGame = () => {
+playBtn = () => {
+  const myTop = Math.floor(Math.random() * (850 - 85) + 85);
+  const myLeft = Math.floor(Math.random() * (1730 - 0) + 0);
+  newTopValue = myTop + "px";
+  newLeftValue = myLeft + "px";
+  gameBtn.style.top = newTopValue;
+  gameBtn.style.left = newLeftValue;
+  decreaseSize();
+};
 
-}
+var initalHeight = 100;
+var initalWidth = 200;
+var initalFont = 30;
 
-startBtn.addEventListener('click', startGame)
-gameBtn.addEventListener('click', playBtn)
+decreaseSize = () => {
+  initalFont = initalFont * 0.9;
+  initalHeight = initalHeight - 5;
+  initalWidth = initalWidth - 10;
+
+  var updatedFont = initalFont + "px";
+  var updatedHeight = initalHeight + "px";
+  var updatedWidth = initalWidth + "px";
+
+  console.log(updatedHeight);
+  gameBtn.style.fontSize = updatedFont;
+  gameBtn.style.height = updatedHeight;
+  gameBtn.style.width = updatedWidth;
+};
+
+var i = 0;
+scoreFunction = () => {
+  document.getElementById("myScore").value = ++i;
+};
+
+displayScoreBoard = () => {};
+
+restartGame = () => {};
+
+startBtn.addEventListener("click", startGame);
+gameBtn.addEventListener("click", playBtn);
+
+const countDownTimer = document.getElementById("myCountdown");
+var seconds = 30;
+
+myCountDown = () => {
+  seconds = seconds;
+  seconds--;
+  countDownTimer.value = seconds;
+};
+
+
